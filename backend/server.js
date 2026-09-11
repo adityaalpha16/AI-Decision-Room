9e4891a8fa7793a4db1229f0c9badb890d9ea1bc
adityaalpha16 <adityaalpha16@gmail.com>
2026-09-11 20:43:32 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
